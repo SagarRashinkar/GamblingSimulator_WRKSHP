c8b28ba0bd6db3d4ea28030a0276fd8f004310ad
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-16 21:48:20 +0530

    Added: Win or Lose by 50% then resign

--- src/com/bridgelabz/GamblerSimulator.java
+++ src/com/bridgelabz/GamblerSimulator.java
@@ -12,13 +12,34 @@ public class GamblerSimulator {
 
     public static void main(String[] args) {
 
+        int winAmt = 0;
+        int loseAmt = 0;
+        boolean status = false;
         System.out.println("Starting Stake Value is: "+stake+" $");
         System.out.println("Gambler bet Daily: "+dailyBetValue+" $");
-        int check = checkWinOrLose();
-        if(check == 1){
-            System.out.println("Win!!!");
-        }else {
-            System.out.println("Loose!!!");
+
+        for(int i=1; i<=stake; i++){
+            int check = checkWinOrLose();
+            if(check == 1){
+                winAmt++;
+                if(winAmt == 50){
+                    status = true;
+                    System.out.println("Player win 50% and resigned...");
+                    break;
+                }
+            }else {
+                loseAmt++;
+                if (loseAmt == 50){
+                    status = false;
+                    System.out.println("Player lose by 50% and resigned...");
+                    break;
+                }
+            }
+        }
+        if (status){
+            System.out.println("Final remaining stake is: "+(stake+winAmt));
+        }else{
+            System.out.println("Final remaining stake is: "+(stake-loseAmt));
         }
     }
 }
