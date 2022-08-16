8bb74d017f30645bb5ecc4db5b1d6638da8f69e9
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-17 00:14:38 +0530

    Added: Daily Win or Lose Amount functionality

--- src/com/bridgelabz/GamblerSimulator.java
+++ src/com/bridgelabz/GamblerSimulator.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 public class GamblerSimulator {
 
     public static int stake = 100;
-    public static int dailyBetValue = 1;
+    public static final int dailyBetValue = 1;
 
     public static int checkWinOrLose(){
         int check = (int)(Math.floor(Math.random() * 10)%2);
@@ -18,28 +18,36 @@ public class GamblerSimulator {
         System.out.println("Starting Stake Value is: "+stake+" $");
         System.out.println("Gambler bet Daily: "+dailyBetValue+" $");
 
-        for(int i=1; i<=stake; i++){
-            int check = checkWinOrLose();
-            if(check == 1){
-                winAmt++;
-                if(winAmt == 50){
-                    status = true;
-                    System.out.println("Player win 50% and resigned...");
-                    break;
-                }
-            }else {
-                loseAmt++;
-                if (loseAmt == 50){
-                    status = false;
-                    System.out.println("Player lose by 50% and resigned...");
-                    break;
+        for(int day=1; day<=20; day++){
+            int todayWinAmt = 0;
+            int todayLoseAmt = 0;
+
+            for(int i=1; i<=stake; i++){
+                int check = checkWinOrLose();
+                if(check == 1){
+                    winAmt++;
+                    todayWinAmt++;
+                    if(winAmt == 50){
+                        winAmt = 0;
+                        status = true;
+                        System.out.println("Player win 50% and resigned...");
+                        break;
+                    }else if (loseAmt == 50){
+                        loseAmt = 0;
+                        status = false;
+                        System.out.println("Player lose by 50% and resigned...");
+                        break;
+                    }
+                }else {
+                    loseAmt++;
+                    todayLoseAmt++;
                 }
             }
-        }
-        if (status){
-            System.out.println("Final remaining stake is: "+(stake+winAmt));
-        }else{
-            System.out.println("Final remaining stake is: "+(stake-loseAmt));
+            if (status){
+                System.out.println("Day "+day+" winning Amount is: "+(todayWinAmt));
+            }else{
+                System.out.println("Day "+day+" losing Amount is: "+(todayLoseAmt));
+            }
         }
     }
 }
