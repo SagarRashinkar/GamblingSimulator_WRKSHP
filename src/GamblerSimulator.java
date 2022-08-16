777cbf30126d059825cbf1e0b63d09fae7428588
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-16 20:39:31 +0530

    Added: Start with give stake value

--- src/GamblerSimulator.java
+++ src/GamblerSimulator.java
@@ -1,13 +1,13 @@
 public class GamblerSimulator {
 
+    public static int stake = 100;
+    public static int dailyBetValue = 1;
     public static void welcomeMsg(){
         System.out.println("Welcome to Gambler Simulator Program...");
     }
 
     public static void main(String[] args) {
 
-        int stake = 100;
-        int dailyBetValue = 1;
         welcomeMsg();
         System.out.println("Starting Stake Value is: "+stake);
         System.out.println("Gambler bet Daily: "+dailyBetValue);
